b125a3ae546ba06ee7cd18c5852b2502247f6308
Harry Mee <harrymee94@gmail.com>
2024-01-16 14:49:59 +0000

    add typesafe resolveers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { resolvers, typeDefs } from './schema/index'
 
 import Fastify from 'fastify'
+import { codegenMercurius } from 'mercurius-codegen'
 import mercurius from 'mercurius'
 
 export const app = Fastify({
@@ -11,5 +12,9 @@ app.register(mercurius, {
   schema: typeDefs,
   resolvers,
 })
+
+codegenMercurius(app, {
+  targetPath: './src/schema/generated.ts',
+}).catch(console.error)
 
 app.listen({ port: 8000 })
